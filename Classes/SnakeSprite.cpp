5200c801c31cd7923c4cbfa10cc50665b6f0681b
Robert Bartlensky <bartlensky.robert@gmail.com>
2017-01-17 22:08:18 +0000

    Create visual representation of the Snake.

---
+++ Classes/SnakeSprite.cpp
@@ -0,0 +1,41 @@
+#include "SnakeSprite.h"
+
+USING_NS_CC;
+
+#define COCOS2D_DEBUG 1
+
+SnakeSprite::SnakeSprite(const Size& sceneSize, float segmentWidth, float segmentHeight)
+: Sprite(), _snake(new Snake(sceneSize, segmentWidth, segmentHeight)) {
+
+    const vector<Vec2> snake = _snake->getSnake();
+
+    for (size_t i = 0; i < snake.size(); ++i) {
+
+        Sprite* segmentSprite = i == 0 ? Sprite::create("head.png") : Sprite::create("segment.png");
+        segmentSprite->setAnchorPoint(Vec2::ZERO);
+        segmentSprite->setPosition(snake[i]);
+        addChild(segmentSprite);
+    }
+}
+
+SnakeSprite::~SnakeSprite() {
+
+    delete _snake;
+}
+
+void SnakeSprite::update() {
+
+    _snake->update();
+    vector<Vec2> segments = _snake->getSnake();
+    Vector<Node*> children = getChildren();
+
+    for (int i = 0; i < this->getChildrenCount(); ++i) {
+
+        children.at(i)->setPosition(segments[i]);
+    }
+}
+
+void SnakeSprite::changeDirection(int direction) {
+
+    _snake->changeDirection(direction);
+}
---
+++ Classes/SnakeSprite.h
@@ -0,0 +1,26 @@
+#ifndef SNAKESPRITE_H_INCLUDED
+#define SNAKESPRITE_H_INCLUDED
+
+#include "cocos2d.h"
+#include "logic/Snake.h"
+
+class SnakeSprite : public cocos2d::Sprite {
+
+public:
+
+    SnakeSprite(const cocos2d::Size& sceneSize, float segmentWidth, float segmentHeight);
+
+    virtual ~SnakeSprite();
+
+    virtual void update();
+
+    void changeDirection(int direction);
+
+    static SnakeSprite* create(const cocos2d::Size& sceneSize, float segmentWidth, float segmentHeight);
+
+private:
+
+    Snake* _snake;
+};
+
+#endif // SNAKESPRITE_H_INCLUDED
